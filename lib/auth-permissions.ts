ff051c6a29becf0aabe3376653c555497903b4c8
Joakim Rosenfeldt Pedersen <joakim@mellonn.com>
2026-09-14 14:31:36 +0200

    restrict date label printing for members

--- lib/auth-permissions.ts
+++ lib/auth-permissions.ts
@@ -201,7 +201,6 @@ export const defaultRolePermissions: Record<
       id !== "sales.viewDetail",
   ),
   member: [
-    "dateLabels.print",
     "count.register",
     "count.viewStock",
     "waste.register",
